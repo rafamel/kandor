9ae0d9e454e6ccdab1e471086f13578457ff9cf3
Rafa Mel <melrcm@gmx.net>
2019-10-01 17:57:25 +0200

    feat(core/create): adds select

--- packages/core/src/create/index.ts
+++ packages/core/src/create/index.ts
@@ -1,6 +1,7 @@
 export { default as collection } from './collection';
 export { default as group } from './group';
 export { default as hook } from './hook';
+export { default as select } from './select';
 export { default as root } from './root';
 export { default as schema } from './schema';
 export { default as scope } from './scope';
