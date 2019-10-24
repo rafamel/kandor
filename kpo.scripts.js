ea24ddfd865e2467a51422bb12eb0d7726dba8c4
Rafa Mel <melrcm@gmx.net>
2019-10-24 23:21:12 +0200

    build: adds root build script

--- kpo.scripts.js
+++ kpo.scripts.js
@@ -1,9 +1,11 @@
 const { scripts } = require('./project.config');
+const { kpo } = require('kpo');
 
 module.exports.scripts = {
   ...scripts,
-  bootstrap: 'lerna bootstrap',
+  bootstrap: ['lerna bootstrap', kpo`build`],
   link: 'lerna link',
+  build: [kpo`@core build`, kpo`@rest-adapter build`, kpo`@rpc-adapter build`],
 
   /* Hooks */
   postinstall: 'kpo bootstrap'
