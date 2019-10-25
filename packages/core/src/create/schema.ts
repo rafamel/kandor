8b947e5b511688fb4fdc2bb5300a2d4d2a67fc08
Rafa Mel <melrcm@gmx.net>
2019-10-25 17:14:23 +0200

    feat(core/create): schema allows to specify additionalProperties; schema only sets type as "object"

--- packages/core/src/create/schema.ts
+++ packages/core/src/create/schema.ts
@@ -1,11 +1,20 @@
 import { Schema } from '~/types';
 
+export function schema(schema?: Schema): Schema;
+export function schema(additionalProperties: boolean, schema?: Schema): Schema;
+
 /**
- * Returns the input `schema`, as a type `'object'` if it was not specified. It exists to serve as a helper to improve type inference.
+ * Returns the input `schema`. If `additionalProperties` is provided as a first argument, it will also set the schema `type` to `"object"`, and its `additionalProperties` to the value provided.
  */
-export function schema(schema: Schema = {}): Schema {
-  return {
-    type: schema.type || 'object',
-    ...schema
-  };
+export function schema(a?: Schema | boolean, b?: Schema): Schema {
+  const hasAdditionalProperties = typeof a === 'boolean';
+  const schema = (hasAdditionalProperties ? b : (a as Schema)) || {};
+
+  return hasAdditionalProperties
+    ? {
+        type: schema.type || 'object',
+        ...schema,
+        additionalProperties: a as boolean
+      }
+    : schema;
 }
