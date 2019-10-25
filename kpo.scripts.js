a81fa3f467e21a0e3212592ff130c8721e6c473a
Rafa Mel <melrcm@gmx.net>
2019-10-25 11:40:51 +0200

    build: adds rpc-client to build script

--- kpo.scripts.js
+++ kpo.scripts.js
@@ -5,7 +5,12 @@ module.exports.scripts = {
   ...scripts,
   bootstrap: ['lerna bootstrap', kpo`build`],
   link: 'lerna link',
-  build: [kpo`@core build`, kpo`@rest-adapter build`, kpo`@rpc-adapter build`],
+  build: [
+    kpo`@core build`,
+    kpo`@rest-adapter build`,
+    kpo`@rpc-adapter build`,
+    kpo`@rpc-client build`
+  ],
 
   /* Hooks */
   postinstall: 'kpo bootstrap'
