8756264f97212563404bf5c464c087b87d627545
Rafa Mel <melrcm@gmx.net>
2019-10-29 18:42:08 +0100

    build: builds only common packages on bootstrap

--- kpo.scripts.js
+++ kpo.scripts.js
@@ -3,16 +3,19 @@ const { kpo } = require('kpo');
 
 module.exports.scripts = {
   ...scripts,
-  bootstrap: ['lerna bootstrap', kpo`build`],
+  bootstrap: ['lerna bootstrap', kpo`build:common`],
   link: 'lerna link',
   build: [
-    kpo`@core build`,
-    kpo`@intercepts build`,
-    kpo`@rest build`,
-    kpo`@rpc build`,
+    kpo`build:common`,
     kpo`@http-adapter build`,
     kpo`@ws-adapter build`,
     kpo`@ws-client build`
+  ],
+  'build:common': [
+    kpo`@core build`,
+    kpo`@intercepts build`,
+    kpo`@rest build`,
+    kpo`@rpc build`
   ],
 
   /* Hooks */
