e6385d2c03f0dd2adfc1cf9c904a255b0b3fc5a4
Rafa Mel <melrcm@gmx.net>
2019-11-23 23:36:35 +0100

    fix(core): exports generate functions and PublicError from entry point

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,7 @@
 export * from './classes';
+export * from './generate';
 export * from './inspect';
 export * from './transform';
 export * from './utils';
 export * from './types';
+export * from './PublicError';
